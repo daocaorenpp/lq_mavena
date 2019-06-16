c584de4689b380502a0623c9314097281ee76b5d
wangjiefeng <jiefengfire@163.com>
2019-06-16 17:08:08 +0800

    Initial commit

---
+++ src/main/java/com/fire/test/Hello.java
@@ -0,0 +1,11 @@
+package com.fire.test;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+    public void hello(){
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/com/fire/test/Test01.java
@@ -0,0 +1,11 @@
+package com.fire.test;
+
+import org.junit.Test;
+
+public class Test01 {
+    @Test
+    public void test(){
+        Hello hello = new Hello();
+        hello.hello();
+    }
+}
